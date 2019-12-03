383a0207b273f598f61715b0d556f03f3c067188
Felix Liu <felixliu.hk@gmail.com>
2019-12-03 02:11:56 -0500

    Add files via upload

---
+++ Assets/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#fullpage").fullpage({
+        licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+        scrollOverflow: true
+        // licenseKey: "468D9499-76EC47CB-8F26A509-D52B586B"
+    });
+});
